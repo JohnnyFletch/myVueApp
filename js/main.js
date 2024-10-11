eda008869ac03edac00c61ec44c25addf009e165
JohnnyFletch <johnny.cage@mail.ru>
2024-10-11 15:01:23 +0300

    Вторая версия игры

--- js/main.js
+++ js/main.js
@@ -178,27 +178,3 @@ let app = new Vue({
         }
     }            
 })
-
-let audio = new Audio('audio/soundtrack.mp3');
-let audio__btn = document.querySelector('.btn__sound');
-let audio__icon = document.querySelector('.btn__sound i');
-
-audio.muted = true;
-audio.autoplay = true;
-audio.volume = 0.4;
-
-audio.addEventListener('loadmetadata', function() {
-    audio.currentTime = 0 + Math.random() * (audio.duration + 1 - 0);
-})
-
-audio__btn.addEventListener('click', function() {
-    if(audio.muted) {
-        audio.muted = false;
-        audio__icon.classList.add('fa-volume-up');
-        audio__icon.classList.remove('fa-volume-off');
-    } else if(!audio.muted) {
-        audio.muted = true;
-        audio__icon.classList.remove('fa-volume-up');
-        audio__icon.classList.add('fa-volume-off');
-    }
-})
